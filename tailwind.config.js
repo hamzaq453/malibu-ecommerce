e38772abe10d9dffc569c96b36924e0ec26d6673
waleedmajeed001 <waleedmajeed630@gmail.com>
2025-06-04 16:57:23 +0500

    productdetails page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,17 @@ module.exports = {
       extend: {
         animation: {
           'slide-in': 'slideIn 0.3s ease-out forwards',
+          slideDown: 'slideDown 0.3s ease-out forwards',
         },
         keyframes: {
           slideIn: {
             '0%': { transform: 'translateX(-100%)' },
             '100%': { transform: 'translateX(0)' },
           },
+          slideDown: {
+            '0%': { transform: 'translateY(-10px)', opacity: '0' },
+            '100%': { transform: 'translateY(0)', opacity: '1' },
+          }
         },
       },
     },
